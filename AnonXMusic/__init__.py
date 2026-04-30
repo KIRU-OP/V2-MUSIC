69578c88206681b36dcda0d66b4838cee446903c
кιяυ [ 𝚂ʌηᴧᴛᴧηɪ ] <np564605@gmail.com>
2026-04-30 13:58:19 +0530

    Initialize directories and add JioSaavn API
    Initialized directories, git, database, and heroku. Added JioSaavn API initialization.

--- AnonXMusic/__init__.py
+++ AnonXMusic/__init__.py
@@ -6,21 +6,25 @@ from AnonXMusic.misc import dbb, heroku
 
 from .logging import LOGGER
 
+# Initialize directories, git, database, and heroku
 dirr()
 git()
 dbb()
 heroku()
 
+# Initialize Bot and Userbot
 app = Anony()
 userbot = Userbot()
 
+# Import Platform APIs
 from .platforms import *
 
 Apple = AppleAPI()
-Jiosabun = Jiosabun()
 Carbon = CarbonAPI()
 SoundCloud = SoundAPI()
 Spotify = SpotifyAPI()
 Resso = RessoAPI()
 Telegram = TeleAPI()
 YouTube = YouTubeAPI()
+# Correct the class name if necessary (usually SaavnAPI or JioSaavnAPI)
+JioSaavn = SaavnAPI()
